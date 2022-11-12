51bb345654949f36dd924c4c8842c49a3b03cdde
Игорь Михайлович Панасеня <panasenya1995@mail.ru>
2022-11-12 23:45:18 +0300

    разметка и адаптив

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#f3f3f3',
+        secondary: '#b2b5b7',
+        activeColor: '#e14f59',
+        bgColor: '#090909',
+        bgBoxColor: '#0e1011',
+      },
+      fontFamily: {
+        exo: ['Exo', 'sans-serif'],
+      },
+      container: {
+        padding: '20px',
+        center: true
+      },
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1700px',
+      },
+      boxShadow: {
+        'xl': '0 0 10px #22d3ee',
+        '2xl': '0 0 20px #22d3ee',
+      },
+    },
+  },
+  plugins: [],
+}
